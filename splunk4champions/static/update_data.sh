d2bf9541052b46840e4b6d2324bba40a53463e66
bautt <tbaublys@gmail.com>
2021-11-25 12:15:32 +0100

    Scripted daily data update Thu 25 Nov 12:15:32 CET 2021

--- splunk4champions/static/update_data.sh
+++ splunk4champions/static/update_data.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 # update DAX data 
-echo "starting" 
+echo "starting at `date`"
 cd /home/pi/splunk4champions/splunk4champions/static
  /usr/bin/python3 s4c_getdata.py 
-  sleep 30
-
    #update weather archive
+echo "starting weather tar.g at `date`"
     /usr/bin/python3 s4c_getweather.py
-     sleep 300
-
       # github upload
-
+echo "starting git stuf at `date`"
       /usr/bin/git -C /home/pi/splunk4champions/splunk4champions/static add .
       /usr/bin/git -C /home/pi/splunk4champions/splunk4champions/static pull
       /usr/bin/git -C /home/pi/splunk4champions/splunk4champions/static commit -m "Scripted daily data update `date`"
